479e17ac08b3e6d9dba4a7e886daaf8ed193263a
Template Janitor <template.janitor@example.com>
2026-01-15 16:30:39 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(lyra_supports_sanitizers)
   # Emscripten doesn't support sanitizers
   if(EMSCRIPTEN)
     set(SUPPORTS_UBSAN OFF)
@@ -58,113 +58,113 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(lyra_setup_options)
+  option(lyra_ENABLE_HARDENING "Enable hardening" ON)
+  option(lyra_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    lyra_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    lyra_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  lyra_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR lyra_PACKAGING_MAINTAINER_MODE)
+    option(lyra_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(lyra_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(lyra_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(lyra_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(lyra_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(lyra_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(lyra_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(lyra_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(lyra_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(lyra_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(lyra_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(lyra_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(lyra_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(lyra_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(lyra_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(lyra_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(lyra_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(lyra_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(lyra_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(lyra_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(lyra_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(lyra_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(lyra_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(lyra_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(lyra_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(lyra_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      lyra_ENABLE_IPO
+      lyra_WARNINGS_AS_ERRORS
+      lyra_ENABLE_USER_LINKER
+      lyra_ENABLE_SANITIZER_ADDRESS
+      lyra_ENABLE_SANITIZER_LEAK
+      lyra_ENABLE_SANITIZER_UNDEFINED
+      lyra_ENABLE_SANITIZER_THREAD
+      lyra_ENABLE_SANITIZER_MEMORY
+      lyra_ENABLE_UNITY_BUILD
+      lyra_ENABLE_CLANG_TIDY
+      lyra_ENABLE_CPPCHECK
+      lyra_ENABLE_COVERAGE
+      lyra_ENABLE_PCH
+      lyra_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  lyra_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (lyra_ENABLE_SANITIZER_ADDRESS OR lyra_ENABLE_SANITIZER_THREAD OR lyra_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(lyra_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(lyra_global_options)
+  if(lyra_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    lyra_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  lyra_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(lyra_ENABLE_HARDENING AND lyra_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR lyra_ENABLE_SANITIZER_UNDEFINED
+       OR lyra_ENABLE_SANITIZER_ADDRESS
+       OR lyra_ENABLE_SANITIZER_THREAD
+       OR lyra_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${lyra_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${lyra_ENABLE_SANITIZER_UNDEFINED}")
+    lyra_enable_hardening(lyra_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(lyra_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(lyra_warnings INTERFACE)
+  add_library(lyra_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  lyra_set_project_warnings(
+    lyra_warnings
+    ${lyra_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
@@ -172,72 +172,72 @@ macro(myproject_local_options)
 
   # Linker and sanitizers not supported in Emscripten
   if(NOT EMSCRIPTEN)
-    if(myproject_ENABLE_USER_LINKER)
+    if(lyra_ENABLE_USER_LINKER)
       include(cmake/Linker.cmake)
-      myproject_configure_linker(myproject_options)
+      lyra_configure_linker(lyra_options)
     endif()
 
     include(cmake/Sanitizers.cmake)
-    myproject_enable_sanitizers(
-      myproject_options
-      ${myproject_ENABLE_SANITIZER_ADDRESS}
-      ${myproject_ENABLE_SANITIZER_LEAK}
-      ${myproject_ENABLE_SANITIZER_UNDEFINED}
-      ${myproject_ENABLE_SANITIZER_THREAD}
-      ${myproject_ENABLE_SANITIZER_MEMORY})
+    lyra_enable_sanitizers(
+      lyra_options
+      ${lyra_ENABLE_SANITIZER_ADDRESS}
+      ${lyra_ENABLE_SANITIZER_LEAK}
+      ${lyra_ENABLE_SANITIZER_UNDEFINED}
+      ${lyra_ENABLE_SANITIZER_THREAD}
+      ${lyra_ENABLE_SANITIZER_MEMORY})
   endif()
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(lyra_options PROPERTIES UNITY_BUILD ${lyra_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(lyra_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      lyra_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(lyra_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    lyra_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(lyra_ENABLE_CLANG_TIDY)
+    lyra_enable_clang_tidy(lyra_options ${lyra_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(lyra_ENABLE_CPPCHECK)
+    lyra_enable_cppcheck(${lyra_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(lyra_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    lyra_enable_coverage(lyra_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(lyra_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(lyra_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(lyra_ENABLE_HARDENING AND NOT lyra_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR lyra_ENABLE_SANITIZER_UNDEFINED
+       OR lyra_ENABLE_SANITIZER_ADDRESS
+       OR lyra_ENABLE_SANITIZER_THREAD
+       OR lyra_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    lyra_enable_hardening(lyra_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
